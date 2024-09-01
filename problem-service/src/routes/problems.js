656abf69ae05ff2cec62323c32aaf4109b728f9e
ntua-el20865 <149335417+ntua-el20865@users.noreply.github.com>
2024-09-01 15:33:29 +0300

    Add files via upload

--- problem-service/src/routes/problems.js
+++ problem-service/src/routes/problems.js
@@ -1,12 +1,7 @@
-// problem-service/src/routes/problems.js
 const express = require('express');
-const { submitProblem, getProblems, getStatistics, solveProblem } = require('../controllers/problemController');
-
+const { submitProblem, deleteProblem } = require('../controllers/problemController');
 const router = express.Router();
 
 router.post('/submit-problem', submitProblem);
-router.get('/', getProblems);
-router.get('/statistics', getStatistics);
-router.post('/solve/:id', solveProblem);
-
+router.delete('/delete-problem/:id', deleteProblem);
 module.exports = router;
