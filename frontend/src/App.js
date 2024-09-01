c39e8535f3c56416cb9464422f4fdb3d2ff2cde2
ntua-el20865 <149335417+ntua-el20865@users.noreply.github.com>
2024-09-01 15:31:31 +0300

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 // src/App.js
 import React from 'react';
-import BuyCreditsForm from './components/BuyCreditsForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import AdminPage from './components/AdminPage';
+import SubmissionResults from './components/SubmissionResults';
+import UserSubmissions from './components/UserSubmissions';
+import BuyCreditsPage from './components/BuyCreditsPage';
+import SubmitProblemPage from './components/SubmitProblemPage';
 import SubmitProblemForm from './components/SubmitProblemForm';
 import ManageSolutions from './components/ManageSolutions';
 import ProblemsList from './components/ProblemsList';
@@ -8,9 +14,27 @@ import Statistics from './components/Statistics';
 
 const App = () => {
     return (
+        <Router>
+            <div className="app">
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/admin" element={<AdminPage />} />
+                    <Route path="/results/:id" element={<SubmissionResults />} /> {/* Update this line */}
+                    <Route path="/user" element={<UserSubmissions />} />
+                    <Route path="/buy-credits" element={<BuyCreditsPage />} />
+                    <Route path="/submit-problem" element={<SubmitProblemPage />} />
+                    <Route path="/application" element={<Application />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
+
+// Component for the application after the LandingPage
+const Application = () => {
+    return (
         <div>
             <h1>Problem Solving Application</h1>
-            <BuyCreditsForm />
             <SubmitProblemForm />
             <ManageSolutions />
             <ProblemsList />
