c603a468344a6323718850da216aef9068672254
ntua-el20634 <147844594+ntua-el20634@users.noreply.github.com>
2024-09-18 19:12:27 +0300

    Update db.js

--- problem-service/src/db.js
+++ problem-service/src/db.js
@@ -1,14 +1,11 @@
-// problem-service/src/db.js
 const { Pool } = require('pg');
-require('dotenv').config(); // Φόρτωση μεταβλητών περιβάλλοντος
-
-// const { Pool } = require('pg');
+require('dotenv').config(); // Load environment variables
 
 const pool = new Pool({
     user: process.env.POSTGRES_USER || 'postgres',
     host: process.env.POSTGRES_HOST || 'localhost',
     database: process.env.POSTGRES_DB || 'mydatabase',
-    password: process.env.POSTGRES_PASSWORD || 'Admin',
+    password: process.env.POSTGRES_PASSWORD || '2372002',
     port: process.env.POSTGRES_PORT || 5433,
 });
 
