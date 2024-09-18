0ccf43690aaff580b28ba75fa8c38f6b58f3836c
ntua-el20634 <147844594+ntua-el20634@users.noreply.github.com>
2024-09-18 19:09:57 +0300

    Update index.js

--- payment-service/src/index.js
+++ payment-service/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const paymentRoutes = require('./routes/paymentRoutes'); // Ensure this path is correct
+const pool = require('./db');
 
 const app = express();
 const port = process.env.PORT || 3002;
