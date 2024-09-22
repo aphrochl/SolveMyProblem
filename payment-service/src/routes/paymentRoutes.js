cf3a6868f598696571a4358dd039b58e8bf6562b
ntua-el20889 <130045019+ntua-el20889@users.noreply.github.com>
2024-09-22 19:07:46 +0300

    Update paymentRoutes.js

--- payment-service/src/routes/paymentRoutes.js
+++ payment-service/src/routes/paymentRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { purchaseCredits } = require('../controllers/paymentController');
+const { purchaseCredits, checkCredits, consumeCredits, availableCredits, clearExpiredCredits } = require('../controllers/paymentController');
 
 const router = express.Router();
 
-// Make sure the route matches exactly
+// Purchase credits
 router.post('/purchase-credits', purchaseCredits);
+
+// Consume credits
+router.post('/consume-credits', checkCredits, consumeCredits);
+
+router.get('/available-credits', availableCredits);
+
+//clear the expired credits
+router.post('/clear-credits', clearExpiredCredits);
 
 module.exports = router;
