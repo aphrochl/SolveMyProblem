7b7c31227298ae44878fea982e7b52c8e5a2ebf7
ntua-el20889 <130045019+ntua-el20889@users.noreply.github.com>
2024-09-25 15:26:01 +0300

    Update problems.js

--- problem-service/src/routes/problems.js
+++ problem-service/src/routes/problems.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { submitProblem, deleteProblem, getViewMetadata, getViewData } = require('../controllers/problemController');
+const { submitProblem, deleteProblem, getView, getResults } = require('../controllers/problemController');
 const router = express.Router();
 
 router.post('/submit-problem', submitProblem);
 router.delete('/delete-problem/:id', deleteProblem);
-router.get('/:id/viewMetadata', getViewMetadata);
-router.get('/:id/viewData', getViewData);
+router.get('/:id/results', getResults);
+router.get('/:id/view', getView);
 
 
 module.exports = router;
